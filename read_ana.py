adfc216031022b0ed846a6df5736bcffb29365e4
Stephen Wei <log9527@gmail.com>
2025-07-25 23:10:24 +0800

    增加快寫法

--- read_ana.py
+++ read_ana.py
@@ -3,7 +3,7 @@
 count = 0
 data = []
 with open('reviews.txt', 'r') as f:
-    for line in f:
+    for line in f:                # line是字串 file是整個清單
         data.append(line)
         # if len(data) % 1000 == 0:
         #     print(len(data))
@@ -24,3 +24,35 @@ print(f'留言的平均長度為{sum_len / len(data)}個字')
 # print(line)
 #
 # print(data[10000])
+
+
+# ==================留言的篩選==================
+# 例如：篩選出留言數量小於100的留言
+
+new = []
+for d in data:
+    if len(d) < 100:  # 如果留言長度小於100
+        new.append(d)  # 就加入到new清單中
+
+print('一共有', len(new), "筆留言長度小於100")
+print(new[0])  # 印出第一筆留言
+print(new[1])  # 印出第二筆留言
+
+
+# ==================單字的篩選==================
+good = []
+for d in data:
+    if 'good' in d:  # 如果留言中有good這個單字
+        good.append(d)  # 就加入到good清單中 所以append的d是運算式的結果(d 為篩選過後的字串)
+print('一共有', len(good), "筆留言中有good這個單字")
+# print(good[0])  # 印出第一筆留言
+
+# ==================列表生成式==================
+good = [d for d in data if 'good' in d]  # 等同43~46 (list comprehension)
+# 第一個d為運算式，此處的意思為append到good清單中的append的d
+# print(good)
+print('一共有', len(good), "筆留言中有good這個單字")
+
+
+bad = ['bad' in d for d in data]
+print(bad)
